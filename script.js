12434e6091f543f04f4c5aaa6315fbf090ff25aa
Juan Ma Ortiz <jortize@unal.edu.co>
2026-01-09 17:20:54 -0500

    Highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.number').textContent = secretNumber;
 
@@ -15,6 +16,11 @@ document.querySelector('.check').addEventListener('click', () => {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guessNumber > secretNumber) {
     if (score > 1) {
       document.querySelector('.check').textContent = 'Check!';
