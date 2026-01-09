3b8137bbed63bde79d271d235f70d0148dbe8534
Juan Ma Ortiz <jortize@unal.edu.co>
2026-01-09 16:53:05 -0500

    Checks validaciones

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guessNumber = Number(document.querySelector('.guess').value);
+  if (!guessNumber) {
+    document.querySelector('.check').textContent = 'Pon un número!';
+  } else if (guessNumber === secretNumber) {
+    document.querySelector('.message').textContent = 'Has ganado!';
+  } else if (guessNumber > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.check').textContent = 'Check!';
+      document.querySelector('.message').textContent = 'Muy alto!';
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Has perdido el juego!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guessNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.check').textContent = 'Check!';
+      document.querySelector('.message').textContent = 'Muy bajo!';
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Has perdido el juego!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
